c17ff13dbdd111865cda19f8e5b71d56767c218c
Zhongrui Li <GODLZR@Zhongrui-Lis-MacBook-Pro.local>
2015-03-26 15:35:47 -0400

    Initial Commit

---
+++ CrackingCodingInterviewCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingCodingInterviewCpp
+//
+//  Created by  Zhongrui Li on 2015-03-26.
+//  Copyright (c) 2015  Zhongrui Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
